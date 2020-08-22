9f4483b27f19dbcbdec540d216ade28a12295710
A-Line-of-Code <46698331+A-Line-of-Code@users.noreply.github.com>
2020-08-22 17:24:52 +1200

    Add files via upload

---
+++ Singleton Pattern/firstUse.js
@@ -0,0 +1,8 @@
+import FancyLogger from './fancyLogger.js'
+const logger = new FancyLogger()
+
+export default function logFirstImplementation(){
+    logger.printLogCount()
+    logger.log('First File')
+    logger.printLogCount()
+}
---
+++ Singleton Pattern/secondUse.js
@@ -0,0 +1,8 @@
+import FancyLogger from './fancyLogger.js'
+const logger = new FancyLogger()
+
+export default function logSecondImplementation(){
+    logger.printLogCount()
+    logger.log('Second File')
+    logger.printLogCount()
+}
